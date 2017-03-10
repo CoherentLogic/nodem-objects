4b5d4693d057df261be3f9c9df96838c6f1335ec
John P. Willis <jpw@coherent-logic.com>
2017-03-09 17:12:35 -0700

    Fix get and set

--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,18 @@
-var nodemObjects = require('./lib/nodem-objects');
-var obj = new nodemObjects.Obj({global: "VA", subscripts: [200,1]});
+var nodemObjects = require('../lib/nodem-objects');
+var obj = new nodemObjects.Obj({global: "KBBMTEST", subscripts: []});
 
-console.log(obj.get());
+process.on('uncaughtException', function(err) {
+    console.error(err);
+    obj.close();
+    process.exit(1);
+});
+
+var result = obj.get();
+
+console.log("get result = ", JSON.stringify(result, null, 2));
+
+var setObj = new nodemObjects.Obj({global: "KBBMTEST", subscripts: ["setTest"]});
+
+setObj.set(result);
+
+obj.close();
